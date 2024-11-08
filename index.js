b56d4de34f3f97344a49d8678deea34e00013580
Adelson Castillo <adelsonramon@gmail.com>
2024-11-07 23:01:05 -0600

    renamed file and fixed import

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
-import route from "./routes/employeeRoutes.js"
+import route from "./routes/employeeRoute.js"
 
 
 const app = express()
