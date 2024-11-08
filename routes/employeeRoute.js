aeceb7eca2cff2144fb338946c4b444164731e9a
Adelson Castillo <adelsonramon@gmail.com>
2024-11-07 23:08:05 -0600

    removed create as it was crashing app

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,9 +1,5 @@
 import express from "express"
-import { fetch } from "../controller/employeeController.js"
+import {fetch} from "../controller/employeeController.js"
 const route = express.Router()
-
-route.post("/create", create)
 route.get("/fetch", fetch)
-
-
 export default route
